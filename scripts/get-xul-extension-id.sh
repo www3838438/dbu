430e6b1e2af26fdf02e5bc996e89639e90b980f7
nodiscc <nodiscc@gmail.com>
2016-05-16 20:05:54 +0200

    Make: add get-xul-extension-id.sh script

---
+++ scripts/get-xul-extension-id.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Description: Retrieve the extension id for a firefox addon from its install.rdf
+#Source: http://kb.mozillazine.org/MozillaZine_Knowledge_Base:About
+#License: CC-BY-SA
+
+set -e
+
+EXID=`unzip -qc $1 install.rdf | xmlstarlet sel \
+    -N rdf=http://www.w3.org/1999/02/22-rdf-syntax-ns# \
+    -N em=http://www.mozilla.org/2004/em-rdf# \
+    -t -v \
+    "//rdf:Description[@about='urn:mozilla:install-manifest']/em:id"`
+
+echo "$EXID"
